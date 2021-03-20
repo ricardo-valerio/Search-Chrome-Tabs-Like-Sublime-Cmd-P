dd523b841c6a83561258feade692f7f8b068468a
Ricardo Valério <jricvalerio@gmail.com>
2021-03-20 13:31:59 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Search-Chrome-Tabs-Like-Sublime-Cmd-P
+
+The original extension can be installed here:
+https://chrome.google.com/webstore/detail/sublime-like-tabbing/omlbledfohkfgpgfoklielaeapiomcdk?hl=sv
+
+---
+
+I changed some things that weren't working for me with the original extension. But i give 100% credit to the original developer for this chrome-extension.
+
+I could have changed the `manifest.json` file in order to have the shortcut `Cmd+P` run as default, but i didn't...so remember to go to `chrome://extensions/` and set the shortcut you want to use.
+
+
+- Opened Tabs show up in grey
+- Bookmarked Tabs show up in yellow
+- Tabs in history show up in blue
+
+
+Pressing `Enter` right after typing text opens the first result tab.
+
+To navigate use the `up` and `down` `arrow-keys`.
+
+First time pressing the `down-arrow-key` it starts at the second result tab.
+
+
+![screenshot](https://github.com/rvzzz/Search-Chrome-Tabs-Like-Sublime-Cmd-P/raw/main/screenshot.png)
---
+++ tabwindow.js
@@ -0,0 +1,237 @@
+(function () {
+  'use strict';
+
+   /* global localStorage: false, $: false, chrome: false, Fuse: false */
+
+   /* http://dzone.com/snippets/validate-url-regexp */
+  function isUrl(s) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return regexp.test(s);
+  }
+
+  $(window).load(function(){
+    if (location.search != '?focusHack')location.search = '?focusHack';
+    var $this = $('body'),
+    data = {
+      elem: {
+        inputField: $('#searchFilter', $this).focus(),
+        searchResults: $('#searchResults', $this)
+      },
+      rows: [],
+      linkNoRepeat: []
+    },
+    fuse;
+
+    /* Gather data from tabs */
+     chrome.tabs.query({currentWindow: true}, function(tabs){
+      $.each(tabs, function(index, tab){
+
+        var $elem = $('<li />').html(tab.title + ' <br /> <span class="info">' + tab.url + '</span>').addClass('tab').click(function(){
+          chrome.tabs.update(tab.id, {active: true});
+        });
+
+        data.elem.searchResults.append($elem);
+
+        data.rows.push({
+          title: tab.title,
+          url: tab.url,
+          id: tab.id,
+          elem: $elem
+        });
+
+        data.linkNoRepeat.push(tab.url);
+
+      });
+      updateSearch();
+    });
+
+    /* Gather data from bookmarks */
+    function process_bookmark(bookmarks, child) {
+      $.each(bookmarks, function(index, bookmark){
+          if (bookmark.url && $.inArray(bookmark.url, data.linkNoRepeat) == -1) {
+
+            var $elem = $('<li />').html(bookmark.title + ' <br /> <span class="info">' + bookmark.url + '</span>').addClass('bookmark').click(function(){
+              chrome.tabs.create({
+                url: bookmark.url,
+                active: true
+              });
+            });
+
+            data.rows.push({
+              title: bookmark.title,
+              url: bookmark.url,
+              elem: $elem
+            });
+            data.elem.searchResults.append($elem);
+          }
+          if (bookmark.children) {
+              process_bookmark(bookmark.children, true);
+          }
+      });
+      if(!child){
+        updateSearch();
+      }
+    }
+    chrome.bookmarks.getTree(process_bookmark);
+
+
+    /* Gather data from history */
+    chrome.history.search(
+      {
+        text: '',
+        maxResults: parseInt(localStorage['sublime-like-tabbing-history'], 10) || 100
+      },
+      function(history){
+        $.each(history, function(index, history){
+          if($.inArray(history.url, data.linkNoRepeat) == -1){
+            var $elem = $('<li />').html(history.title + ' <br /> <span class="info">' + history.url + '</span>').addClass('history').click(function(){
+              chrome.tabs.create({
+                url: history.url,
+                active: true
+              });
+            });
+            data.elem.searchResults.append($elem);
+            data.rows.push({
+              title: history.title,
+              url: history.url,
+              id: history.id,
+              elem: $elem
+            });
+          }
+          updateSearch();
+        });
+      }
+    );
+
+    function updateSearch(){
+      fuse = new Fuse(data.rows, {threshold: parseInt(localStorage['sublime-like-tabbing-threshold'], 10) || 0.3, keys: ['title', 'url']});
+    }
+
+    /* Setup the filter function
+    TODO: optimization, i.e. do not hide all and then show.
+     */
+    data.elem.inputField.change(function(){
+      var filterString = $(this).val();
+      if(!filterString){
+        $.each(data.rows, function(index, tab){
+          tab.elem.show();
+        });
+      } else {
+        $(data.elem.searchResults).children().hide();
+        if(fuse){
+          var result = fuse.search(filterString);
+          $.each(result, function(index, row){
+            row.elem.show();
+          });
+        }
+      }
+    }).keyup(function(){
+      $(this).change();
+    });
+
+    var ARROW_DOWN = 40, ARROW_UP = 38, ENTER = 13, ESCAPE = 27;
+
+    /* register key movement and enter */
+    var $liSelected, $allLi;
+    $(window).keydown(function(e){
+
+      if(e.which != ARROW_DOWN && e.which != ARROW_UP && e.which != ENTER && e.which != ESCAPE){
+        return true;
+      }
+
+      if(e.which == ENTER){
+        if(!$liSelected || $liSelected && $liSelected.is(':hidden')){
+          $allLi = $('li:visible', data.elem.searchResults);
+          if($allLi.length > 0){
+            $allLi.first().click();
+          } else {
+            /* url check */
+            if(isUrl(data.elem.inputField.val())){
+              chrome.tabs.create({
+                url: data.elem.inputField.val(),
+                active: true
+              });
+            } else {
+              chrome.tabs.create({
+                url: 'https://www.google.se/search?q=' + data.elem.inputField.val(), // I WANT TO USE OMNIBOX HERE BUT CANT :/ :/ :/
+                active: true
+              });
+            }
+          }
+        } else {
+          $liSelected.click();
+        }
+        window.close();
+        return false;
+      }
+
+      if(e.which == ESCAPE){
+        window.close();
+        return false;
+      }
+
+      if(e.which == ARROW_DOWN || e.which == ARROW_UP){
+        $('li:hidden', data.elem.searchResults).removeClass('selected');
+        $allLi = $('li:visible', data.elem.searchResults);
+        if($liSelected && $liSelected.is(':hidden')){
+          $liSelected = null;
+        }
+        var next;
+        if(e.which == ARROW_DOWN){
+            if($liSelected && $liSelected.next){
+                $liSelected.removeClass('selected');
+                next = $liSelected.nextAll(':visible');
+                if(next.length > 0){
+                    $liSelected = next.first().addClass('selected');
+                }else{
+                    $liSelected = $allLi.first().addClass('selected');
+                }
+            }else{
+                // BEFORE:
+                // $liSelected = $allLi.first().addClass('selected');
+
+                // AFTER:
+                // since pressing Enter, after typing text, opens the 1st tab result...
+                // start the selection at the 2nd line (index 1)
+                $liSelected = $allLi.eq(1).addClass('selected');
+            }
+        }else if(e.which == ARROW_UP){
+            if($liSelected && $liSelected.prev){
+                $liSelected.removeClass('selected');
+                next = $liSelected.prevAll(':visible').first();
+                if(next.length > 0){
+                    $liSelected = next.first().addClass('selected');
+                }else{
+                    $liSelected = $allLi.last().addClass('selected');
+                }
+            }else{
+                $liSelected = $allLi.last().addClass('selected');
+            }
+        }
+        /* scroll to selected element */
+
+        data.elem.searchResults.scrollTop(data.elem.searchResults.scrollTop() + $liSelected.offset().top - 250);
+
+        /* In fact, i don't see a need to use any of this code..it still works fine to me...
+
+        if($liSelected && $liSelected.is(':visible') && $liSelected.hasClass('tab')){
+
+          // BEFORE:
+          // $liSelected.click();
+
+          // AFTER: nothing! :)
+          // hacking way to prevent the bug of opening the first tab
+          // when using ARROW_DOWN key to select the listed tabs
+
+        } else if(!$liSelected || $liSelected && $liSelected.is(':hidden')){
+          $liSelected = $allLi.first().addClass('selected');
+          if($liSelected.hasClass('tab')){
+            $liSelected.click();
+          }
+        }
+        */
+      }
+      return false;
+    });
+  });
+}());
